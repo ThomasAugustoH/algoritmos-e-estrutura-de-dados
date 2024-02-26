a6c5976156ed43cf2df6d3bd0b9074a33ba1d88e
ThomasAugustoH <thomas.a.haskel@gmail.com>
2024-02-25 22:34:13 -0300

    Atualizado Testes

--- Lista01/ListaEstaticaTest.java
+++ Lista01/ListaEstaticaTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class ListaEstaticaTest {
+    private ListaEstatica l;
 
     public ListaEstaticaTest() {
     }
@@ -20,125 +21,67 @@ public class ListaEstaticaTest {
 
     @Before
     public void setUp() {
+        l = new ListaEstatica();
+        l.inserir(5);
+        l.inserir(10);
+        l.inserir(15);
+        l.inserir(20);
     }
 
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of inserir method, of class ListaEstatica.
-     */
     @Test
     public void test01() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
-
         assertEquals("5,10,15,20", l.toString());
     }
 
     @Test
     public void test02() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
-
         assertEquals(4, l.getTamanho());
     }
 
     @Test
     public void test03() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
-
         assertEquals(2, l.buscar(15));
     }
 
     @Test
     public void test04() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
-
         assertEquals(-1, l.buscar(30));
     }
 
     @Test
     public void test05() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
         l.retirar(10);
-
         assertEquals("5,15,20", l.toString());
         assertEquals(3, l.getTamanho());
     }
 
     @Test
     public void test06() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(1);
-        l.inserir(2);
-        l.inserir(3);
-        l.inserir(4);
-        l.inserir(5);
-        l.inserir(6);
-        l.inserir(7);
-        l.inserir(8);
-        l.inserir(9);
-        l.inserir(10);
-        l.inserir(11);
-        l.inserir(12);
-        l.inserir(13);
-        l.inserir(14);
-        l.inserir(15);
-
+        l.liberar();
+        for (int i = 1; i < 16; i++) {
+            l.inserir(i);
+        }
         assertEquals("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15", l.toString());
         assertEquals(15, l.getTamanho());
     }
 
     @Test
     public void test07() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
-
         assertEquals(20, l.obterElemento(3));
     }
 
-    @Test(expected=IndexOutOfBoundsException.class)
+    @Test(expected = IndexOutOfBoundsException.class)
     public void test08() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
         l.obterElemento(5);
     }
 
     @Test
     public void test09() {
-        ListaEstatica l = new ListaEstatica();
-        l.inserir(5);
-        l.inserir(10);
-        l.inserir(15);
-        l.inserir(20);
         l.liberar();
-
         assertEquals(true, l.estaVazia());
     }
 }
