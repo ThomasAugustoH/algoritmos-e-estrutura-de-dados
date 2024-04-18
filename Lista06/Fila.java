cb6cda17134385ccd5caef3e425c6304b40216e4
ThomasAugustoH <thomas.a.haskel@gmail.com>
2024-04-18 18:38:55 -0300

    Adicionada lista 06

---
+++ Lista06/Fila.java
@@ -0,0 +1,14 @@
+package Lista06;
+
+public interface Fila<T> {
+
+    public void inserir(T valor);
+
+    public boolean estaVazia();
+
+    public T peek();
+
+    public T retirar();
+
+    public void liberar();
+}
