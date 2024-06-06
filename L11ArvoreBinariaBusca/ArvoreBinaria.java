6b7ceb91ba4abd827289f136f42bd8686577d09c
ThomasAugustoH <thomas.a.haskel@gmail.com>
2024-06-06 20:28:53 -0300

    Adicionada lista 11

---
+++ L11ArvoreBinariaBusca/ArvoreBinaria.java
@@ -0,0 +1,32 @@
+package L11ArvoreBinariaBusca;
+
+public class ArvoreBinaria<T> extends ArvoreBinariaAbstract<T> {
+
+    public void setRaiz(NoArvoreBinaria<T> raiz) {
+        super.setRaiz(raiz);
+    }
+
+    @Override
+    public NoArvoreBinaria<T> buscar(T info) {
+        return buscar(getRaiz(), info);
+    }
+
+    private NoArvoreBinaria<T> buscar(NoArvoreBinaria<T> no, T info) {
+        if (no == null) {
+            return null;
+        } else {
+            if (no.getInfo().equals(info)) {
+                return no;
+            } else {
+                NoArvoreBinaria<T> noLocalizado;
+
+                noLocalizado = buscar(no.getEsquerda(), info);
+                if (noLocalizado == null) {
+                    noLocalizado = buscar(no.getDireita(), info);
+                }
+
+                return noLocalizado;
+            }
+        }
+    }
+}
---
+++ L11ArvoreBinariaBusca/ArvoreBinariaBuscaTest.java
@@ -0,0 +1,81 @@
+package L11ArvoreBinariaBusca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArvoreBinariaBuscaTest {
+
+    ArvoreBinariaBusca<Integer> arvore = new ArvoreBinariaBusca<>();
+
+    @Test
+    public void teste01() {
+        arvore.inserir(50);
+        arvore.inserir(30);
+        arvore.inserir(70);
+        arvore.inserir(40);
+        arvore.inserir(25);
+        arvore.inserir(75);
+        arvore.inserir(65);
+        arvore.inserir(35);
+        arvore.inserir(60);
+
+        String esperado = "<50<30<25<><>><40<35<><>><>>><70<65<60<><>><>><75<><>>>>";
+
+        assertEquals(esperado, arvore.toString());
+    }
+
+    @Test
+    public void teste02() {
+        arvore.inserir(50);
+        arvore.inserir(30);
+        arvore.inserir(25);
+        arvore.inserir(40);
+
+        arvore.retirar(40);
+
+        String esperado = "<50<30<25<><>><>><>>";
+
+        assertEquals(esperado, arvore.toString());
+    }
+
+    @Test
+    public void teste03() {
+        arvore.inserir(80);
+        arvore.inserir(52);
+        arvore.inserir(90);
+        arvore.inserir(48);
+        arvore.inserir(71);
+        arvore.inserir(63);
+        arvore.inserir(67);
+
+        arvore.retirar(71);
+
+        String esperado = "<80<52<48<><>><63<><67<><>>>><90<><>>>";
+
+        assertEquals(esperado, arvore.toString());
+    }
+
+    @Test
+    public void teste04() {
+        arvore.inserir(250);
+        arvore.inserir(38);
+        arvore.inserir(26);
+        arvore.inserir(72);
+        arvore.inserir(55);
+        arvore.inserir(90);
+        arvore.inserir(41);
+        arvore.inserir(60);
+        arvore.inserir(43);
+        arvore.inserir(78);
+        arvore.inserir(92);
+        arvore.inserir(74);
+
+        arvore.retirar(38);
+
+        String esperado = "<250<41<26<><>><72<55<43<><>><60<><>>><90<78<74<><>><>><92<><>>>>><>>";
+
+        assertEquals(esperado, arvore.toString());
+    }
+}
